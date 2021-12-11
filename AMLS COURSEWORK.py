168404cd814e00800224c60fcee48d8ed3d06e6f
zcemmey <95655700+zcemmey@users.noreply.github.com>
2021-12-11 02:43:39 +0000

    Add files via upload

---
+++ AMLS COURSEWORK.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+import os
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+# In[2]:
+
+
+path = (r"C:\Users\melek\OneDrive\Belgeler\Year 4\AMLS\CW\dataset\image")
+image_list = []
+image_paths = sorted(list(paths.list_images(path)))
+
+
+for image_path in image_paths:
+    grayscale = cvtColor(image_file,cv2.COLOR_RGB2GRAY)
+    image_list.append(file)
+    
+    plt.imshow(img_list[0])
+
+
+# In[ ]:
+
+
+
+
